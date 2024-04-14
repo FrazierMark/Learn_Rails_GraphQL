2e29ed8cdc2743ab599dc4d76697cbcd480fd558
FrazierMark <m.mark.frazier@gmail.com>
2024-04-13 22:54:19 -0500

    validation error update

--- app/graphql/types/validation_error_type.rb
+++ app/graphql/types/validation_error_type.rb
@@ -2,7 +2,8 @@ module Types
   class ValidationErrorType < Types::BaseObject
 
     class Errors < Types::BaseObject
-      field :full_messages, [String], mull: false
+      field :full_messages, [String], null: false
+      field :attribute_errors, [Types::ValidationErrorType::AttributeError], null: false
     end
 
     field :errors, Types::ValidationErrorType::Errors, null: false
